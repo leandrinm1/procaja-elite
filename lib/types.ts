aeda648524f8e7f716e6b9575673477476dbfe56
Leandro Monar <leandrinm8@gmail.com>
2026-02-27 11:26:55 -0500

    Corrigiendo molde Transaccion para Typescript

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,3 @@
 export interface User { id?: string; uid?: string; email?: string | null; displayName?: string | null; role?: string; [key: string]: any; }
 export interface Usuario { id?: string; uid?: string; email?: string | null; nombre?: string | null; rol?: string; [key: string]: any; }
-export interface Transaccion { id?: string; tipo?: string; monto?: number; fecha?: any; descripcion?: string; [key: string]: any; }
+export interface Transaccion { id?: string; tipo: string; monto: number; fecha: any; descripcion?: string; detalle: string; metodoPago: string; [key: string]: any; }
